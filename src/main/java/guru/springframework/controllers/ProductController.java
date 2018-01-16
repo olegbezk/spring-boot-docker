428cf9ee9b401c53cb61e306cb250ee6d0c6c735
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-01-16 16:43:23 +0200

    remove unused imports

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -2,8 +2,6 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.PageViewEventService;
 import guru.springframework.services.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ProductController {
-
-    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     private final ProductService productService;
 
